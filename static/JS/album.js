e77d9f1e1ae0e23a77cf69d8e47d9153396b26be
c23037255 <HockleyB@cardiff.ac.uk>
2024-04-17 18:04:21 +0100

    stopped book at end

--- static/JS/album.js
+++ static/JS/album.js
@@ -7,28 +7,33 @@ console.log(checklist);
 
 function updatePage(){
     if (pageNumber==0){
-        document.getElementById('pageDown').style.display = 'none'
+        document.getElementById('pageDown').style.display = 'none';
         for (i=0;i<6;i++){
-            document.getElementsByClassName('cardSpot')[i].style.display = 'block'
-            document.getElementsByClassName('cardSpot')[i].innerHTML = i+1
-        }
+            document.getElementsByClassName('cardSpot')[i].style.display = 'block';
+            document.getElementsByClassName('cardSpot')[i].innerHTML = i+1;
+        };
         for (i=6;i<16;i++){
-            document.getElementsByClassName('cardSpot')[i].style.display = 'none'
-        }
+            document.getElementsByClassName('cardSpot')[i].style.display = 'none';
+        };
     } else {
-        document.getElementById('pageDown').style.display = 'block'
+        if (pageNumber==24){
+            document.getElementById('pageUp').style.display = 'none';
+        } else {
+            document.getElementById('pageUp').style.display = 'block';
+        };
+        document.getElementById('pageDown').style.display = 'block';
         for (i=0;i<16;i++){
-            document.getElementsByClassName('cardSpot')[i].style.display = 'block'
+            document.getElementsByClassName('cardSpot')[i].style.display = 'block';
             document.getElementsByClassName('cardSpot')[i].innerHTML = (16*pageNumber) - 9 + i;
-        }
-    }
+        };
+    };
     document.getElementById('pageNumber').innerHTML = pageNumber;
-}
+};
 
 document.getElementById('pageDown').addEventListener("click",function(){
     pageNumber -= 1;
     updatePage();
-})
+});
 document.getElementById('pageUp').addEventListener("click",function(){
     pageNumber += 1;
     updatePage();
