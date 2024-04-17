241f868a730697e91f531315ffc9fa9ef1e76073
c23037255 <HockleyB@cardiff.ac.uk>
2024-04-17 16:12:35 +0100

    hide/show slots based on page in album

--- static/JS/album.js
+++ static/JS/album.js
@@ -1,15 +1,32 @@
 var pageNumber = 0; //special stickers
+updatePage();
 checklist = document.getElementsByClassName('hideThis')[0].innerHTML;
 checklist = checklist.slice(2,-3);
 checklist = checklist.split(',), ('); //make string into array (390 items)
 console.log(checklist);
 
-if (pageNumber==0){
-    for (i=0;i<6;i++){
-        document.getElementsByClassName('cardSpot')[i].style.backgroundColor = 'Blue'
-    }
-} else {
-    for (i=0;i<16;i++){
-        document.getElementsByClassName('cardSpot')[i].style.backgroundColor = 'Blue'
+function updatePage(){
+    if (pageNumber==0){
+        document.getElementById('pageDown').style.display = 'none'
+        for (i=0;i<6;i++){
+            document.getElementsByClassName('cardSpot')[i].style.display = 'block'
+        }
+        for (i=6;i<15;i++){
+            document.getElementsByClassName('cardSpot')[i].style.display = 'none'
+        }
+    } else {
+        document.getElementById('pageDown').style.display = 'block'
+        for (i=0;i<15;i++){
+            document.getElementsByClassName('cardSpot')[i].style.display = 'block'
+        }
     }
 }
+
+document.getElementById('pageDown').addEventListener("click",function(){
+    pageNumber -= 1;
+    updatePage();
+})
+document.getElementById('pageUp').addEventListener("click",function(){
+    pageNumber += 1;
+    updatePage();
+})
