2bd1f096c14159688d87d5d75caa491cd3aff185
c23037255 <HockleyB@cardiff.ac.uk>
2024-04-25 11:42:06 +0100

    swap request button

--- static/JS/home.js
+++ static/JS/home.js
@@ -16,14 +16,46 @@ $("#signOut").on("click", function() {
     window.location.href = '/signIn' //signs user out (redirects back to sign in page)
 })
 
+// Get JSON list of all card numbers of user's swaps
 swapList = JSON.parse(document.getElementById('swapList').innerHTML);
 
+// Append corresponding image to swapContainer for card numbers of all the user's swaps.
 for (i=0;i<swapList.length;i++){
-    swap = document.createElement('img');
+    swapBox = document.createElement('div');
+    swapBox.className = 'swapBox';
 
+    //hover node (shows on hover; links to user actions)
+    hoverNode  = document.createElement('div');
+    hoverNode.className = 'hoverNode';
+
+    //create swap request button
+    swapRequestButton = document.createElement('button');
+    swapRequestButton.className = ("btn btn-success swapRequest");
+    swapRequestButton.innerHTML = ("create swap request");
+    hoverNode.appendChild(swapRequestButton);
+
+    swapBox.appendChild(hoverNode);
+
+    //sticker image
+    swap = document.createElement('img');
     swap.classList.add('swap');
     swap.classList.add(swapList[i]);
     swap.setAttribute('src','/static/img/Stickers/'+swapList[i]+'.png');
+    swapBox.appendChild(swap);
 
-    document.getElementById('swapContainer').appendChild(swap);
+    //eventListeners (to display node on hover)
+    swapBox.addEventListener("mouseover",function(){
+        console.log("hover");
+        node = this.firstChild;
+        console.log(node);
+        node.style.display = 'block';
+    });
+    swapBox.addEventListener("mouseout",function(){
+        console.log("out");
+        node = this.firstChild;
+        console.log(node);
+        node.style.display = 'none';
+    })
+
+    document.getElementById('swapContainer').appendChild(swapBox);
 }
